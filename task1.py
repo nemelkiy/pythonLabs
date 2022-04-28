0c98ebe7491784a9d4a84924f0fbfc502df8d2b0
nemelkiy <nemelkiy@gmail.com>
2022-04-28 22:34:37 +0300

    first commit

---
+++ task1.py
@@ -0,0 +1,8 @@
+test_string = 'Just a simple string'
+print('Full string is: ' + test_string)
+print('Slices:')
+print(test_string[-6:])
+print(test_string[0:4])
+print(test_string[7:13])
+print(test_string[::5])
+print(test_string[::-3])
