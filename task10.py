e65eb19657f46aee7f523044e8b2258bfa1d1205
nemelkiy <nemelkiy@gmail.com>
2022-04-30 10:59:22 +0300

    Выборка нескольких повторяющихся чисел

--- task10.py
+++ task10.py
@@ -1,8 +1,18 @@
 def found_letter(lst):
     res = {x:lst.count(x) for x in lst}
-    return max(res, key=res.get)
 
-inpList = ["A", "A", "A", "B", "B", "C", "A"]
+    max_value = 0
+    for key in res:
+        if max_value < res[key]:
+            max_value = res[key]
+    list_most_common = []
+    for key, value in res.items():
+        if value == max_value:
+            list_most_common.append(key)
+    return list_most_common
+    
+inpList = ["A", "A", "A", "B", "B", "C", "A", "B", "B"]
+
 print('\n==========\n')
 print('Входное значение списка:\n')
 print(inpList)
